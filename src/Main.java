4b5f1fe06514db9a88290c0dfd7e38d1ffd0f363
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-27 07:40:55 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
+import java.security.PublicKey;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        /*for (int i =0 ; i <=100; i++) {
+            System.out.println("The number " + i + " " + isOdd(i));
         }
+         */
+
+        System.out.println(sumOdd(1,100));
+        System.out.println(sumOdd(-1,100));
+        System.out.println(sumOdd(100,100));
+        System.out.println(sumOdd(13,13));
+        System.out.println(sumOdd(100,-100));
+        System.out.println(sumOdd(100,1000));
+        System.out.println(sumOdd(10,5));
+    }
+
+    public static boolean isOdd(int number){
+        if(number < 0){
+            return false;
+        }
+        return (number % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end){
+
+        if(start < 0 || end < 0 || start > end) {
+            return -1;
+        }
+
+        int sumOfOddNumbers = 0;
+        for(int i = start; i <= end; i++){
+            if (isOdd(i)){
+                sumOfOddNumbers += i;
+            }
+        }
+        return  sumOfOddNumbers;
     }
 }
